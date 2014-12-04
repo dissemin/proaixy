1f204dd5eee5b9db0204e4c9ee1190cbdcfc9ce5
Antonin Delpeuch <antonin@delpeuch.eu>
2014-12-04 18:20:47 +0100

    Changed the name of the administration pannel

--- proaixy/urls.py
+++ proaixy/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
+admin.site.site_header = 'Proaixy administration'
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
