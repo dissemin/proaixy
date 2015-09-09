118e19d8e69726b1e83a82f7dd31a5993b7d260c
Antonin Delpeuch <antonin@delpeuch.eu>
2015-09-09 12:09:09 +0200

    Separate migrations to create indexes a posteriori

--- oai/migrations/0003_add_indexes.py
+++ oai/migrations/0003_add_indexes.py
@@ -7,18 +7,18 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('oai', '0001_initial'),
+        ('oai', '0002_add_fingerprint_and_doi'),
     ]
 
     operations = [
-        migrations.AddField(
+        migrations.AlterField(
             model_name='oairecord',
             name='doi',
-            field=models.CharField(max_length=512, null=True, blank=True),
+            field=models.CharField(db_index=True, max_length=512, null=True, blank=True),
         ),
-        migrations.AddField(
+        migrations.AlterField(
             model_name='oairecord',
             name='fingerprint',
-            field=models.CharField(max_length=64, null=True),
+            field=models.CharField(max_length=64, null=True, db_index=True),
         ),
     ]
